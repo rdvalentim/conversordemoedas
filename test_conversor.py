82320a75988e9eb376c2f1f9b0775210be50bcf3
rdvalentim <ramon.valentim429@gmail.com>
2025-02-23 14:51:38 -0300

    teste

--- test_conversor.py
+++ test_conversor.py
@@ -2,6 +2,6 @@ import pytest
 from conversordemoedas.conversor import conversor
 
 def test_conversao_valida(monkeypatch):
-    # Simulando a entrada
-    monkeypatch.setattr('builtins.input', lambda: "10,USD,BRL")
-    assert conversor(10, 'USD', 'BRL') == 50  # Ajuste conforme a lógica de conversão
+    # Simulando a entrada corretamente
+    monkeypatch.setattr('builtins.input', lambda prompt: "10,USD,BRL")
+    assert conversor(10, 'USD', 'BRL') == 50  # Ajuste conforme necessário
