67375b17a707a6f0319d43785aed6ff995a46519
rdvalentim <ramon.valentim429@gmail.com>
2025-02-23 14:43:43 -0300

    teste

--- test_conversor.py
+++ test_conversor.py
@@ -1,5 +1,5 @@
 import pytest
-from conversordemoedas import conversor  # type: ignore # Ajuste para o nome real da função
+from conversordemoedas.conversor import conversor
 
 def test_conversor_valida():
     assert conversor(10, 'USD', 'BRL') == 50  # Exemplo de teste, ajuste conforme necessário
